5855bdef1cca306cea263a6ee559f6f647832bde
Haider Ali Punjabi <haiderali@cyberservices.com>
2019-06-06 07:50:43 +0530

    Testing Site Config

--- config/site.js
+++ config/site.js
@@ -11,7 +11,7 @@ module.exports = {
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@emilia', // Twitter Username
   ogSiteName: 'emilia', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
